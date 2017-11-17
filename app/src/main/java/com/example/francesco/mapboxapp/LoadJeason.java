fcaaff709c48fdf569f32bb8d13f70b70e178b71
pietro di marco <pietrodimarco91@gmail.com>
2017-11-17 11:35:37 -0600

    appcontext+Load

---
+++ app/src/main/java/com/example/francesco/mapboxapp/LoadJeason.java
@@ -0,0 +1,35 @@
+package com.example.francesco.mapboxapp;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.channels.FileChannel;
+
+/**
+ * Created by pietrodimarco on 11/17/17.
+ */
+
+public class LoadJeason {
+
+
+
+    public String loadJSONFromAsset() {
+        String json = null;
+        try {
+            InputStream is = AppContext.getAppContext().getAssets().open("file_name.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+
+
+}
