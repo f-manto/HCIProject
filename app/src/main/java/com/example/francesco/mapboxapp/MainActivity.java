c2ac5f268ff1b2acbe13537cb5983765657241c3
pietro di marco <pietrodimarco91@gmail.com>
2017-11-21 22:31:04 -0600

    implemented algorithm navigation fixed + new thread introduced

--- app/src/main/java/com/example/francesco/mapboxapp/MainActivity.java
+++ app/src/main/java/com/example/francesco/mapboxapp/MainActivity.java
@@ -148,7 +148,7 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+       /* if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
@@ -160,7 +160,7 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_send) {
 
-        }
+        }*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
