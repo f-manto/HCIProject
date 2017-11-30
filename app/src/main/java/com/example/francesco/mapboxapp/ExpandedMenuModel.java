97f70b96b06a7d67437a1319b0bd6c46cb7681dd
pietro di marco <pietrodimarco91@gmail.com>
2017-11-29 20:06:15 -0600

    Works with dest room as str

---
+++ app/src/main/java/com/example/francesco/mapboxapp/ExpandedMenuModel.java
@@ -0,0 +1,23 @@
+package com.example.francesco.mapboxapp;
+
+/**
+ * Created by pietrodimarco on 11/25/17.
+ */
+
+public class ExpandedMenuModel {
+    String iconName = "";
+    int iconImg = -1; // menu icon resource id
+
+    public String getIconName() {
+        return iconName;
+    }
+    public void setIconName(String iconName) {
+        this.iconName = iconName;
+    }
+    public int getIconImg() {
+        return iconImg;
+    }
+    public void setIconImg(int iconImg) {
+        this.iconImg = iconImg;
+    }
+}
